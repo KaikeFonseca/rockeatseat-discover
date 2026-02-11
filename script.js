196e4b6b2d22ae9ffdd7fdb0f0acfc64e1310dc8
Kaike <111296944+KaikeFonseca@users.noreply.github.com>
2026-02-10 23:31:53 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle(`light`)
+
+  const img = document.querySelector("#profile img")
+  const altImg =
+    "Foto de Kaike Fonseca sorrindo, usando camisa preta, cavanhaque e fundo azul"
+
+  if (html.classList.contains(`light`)) {
+    img.setAttribute("src", "./assets/assets/avatar-light.png")
+    img.setAttribute("alt", "Foto LightMode")
+  } else {
+    img.setAttribute("src", "./assets/assets/avatar.png")
+    img.setAttribute("alt", altImg)
+  }
+}
